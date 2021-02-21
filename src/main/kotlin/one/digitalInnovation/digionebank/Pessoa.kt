13601baab051087bb59f7d95ad91c8dc03510dcc
lucasviolao <lucaodoti@gmail.com>
2021-02-21 09:33:02 -0300

    created person class

---
+++ src/main/kotlin/one/digitalInnovation/digionebank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digitalInnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Lucas"
+    var cpf: String = "123.123.123-11"
+}
+
+fun main() {
+    val lucas = Pessoa()
+
+    println(lucas.nome)
+    println(lucas.cpf)
+
+}
